fbbd591f1a67e43e2964cfd2585dee993788cef1
= <=>
2015-04-01 16:09:34 +0100

    Add User with authentication devise gem~

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @posts = Post.by_last_created
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  belongs_to :user
 	scope :by_last_created, -> { order(created_at: :desc) }
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
