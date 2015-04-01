da7a5b84955aa2bccc4c92f317df6fa90818603a
= <=>
2015-04-01 14:15:15 +0100

    Add validation to post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
 	scope :by_last_created, -> { order(created_at: :desc) }
+  validates :title, presence: true, length: {minimum: 5}
+  validates :body, presence: true
 end
