820fdff84055b962d7e0d8fdddc27ebae05c54e3
= <=>
2015-04-01 15:01:08 +0100

    Add Comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
 	scope :by_last_created, -> { order(created_at: :desc) }
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
